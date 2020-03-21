22fb6e7035f1e01c83d5385e29d828e305184154
bahaa-al-deen <bzidan2021@gmail.com>
2020-03-21 13:54:58 +0200

    replace with+eval with a safer eval package + fix package.json

--- src/utils/shouldRenderField.js
+++ src/utils/shouldRenderField.js
@@ -1,18 +1,15 @@
-function shouldRenderField(field, allFormValues) {
-  // console.log(field, allFormValues);
+import safeEval from "safe-eval";
+
+export const shouldRenderField = (field, allFormValues) => {
   if (field.dependencyExpression) {
-    with (allFormValues) {
-      try {
-        return eval(field.dependencyExpression);
-      } catch {
-        console.error(
-          "dependency expression parse failure \n make sure fields that are depended on have 'defaultValue'"
-        );
-        return true;
-      }
+    try {
+      return safeEval(field.dependencyExpression, allFormValues);
+    } catch {
+      console.error(
+        "dependency expression parse failure \n make sure fields that are depended on have 'defaultValue'"
+      );
+      return true;
     }
   }
   return true;
-}
-
-module.exports = shouldRenderField;
+};
